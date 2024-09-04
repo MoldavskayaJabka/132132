3241ae93189e614714985f9587386cf167fa8e81
MoldavskayaJabka <koomarik2007@gmail.com>
2024-09-04 17:10:40 +0300

    Add files via upload

---
+++ Gtst1.cpp
@@ -0,0 +1,32 @@
+﻿#include <gtest/gtest.h>
+#include <vector>
+#include "increase_elements.h"
+
+TEST(IncreaseElementsTest, PositiveIncrease) {
+    std::vector<int> input = { 1, 2, 3, 4, 5 };
+    std::vector<int> expected = { 6, 7, 8, 9, 10 };
+    EXPECT_EQ(increase_elements(input, 5), expected);
+}
+
+TEST(IncreaseElementsTest, ZeroIncrease) {
+    std::vector<int> input = { 1, 2, 3, 4, 5 };
+    std::vector<int> expected = { 1, 2, 3, 4, 5 };
+    EXPECT_EQ(increase_elements(input, 0), expected);
+}
+
+TEST(IncreaseElementsTest, NegativeIncrease) {
+    std::vector<int> input = { 5, 6, 7, 8, 9 };
+    std::vector<int> expected = { 3, 4, 5, 6, 7 };
+    EXPECT_EQ(increase_elements(input, -2), expected);
+}
+
+TEST(IncreaseElementsTest, EmptyVector) {
+    std::vector<int> input = {};
+    std::vector<int> expected = {};
+    EXPECT_EQ(increase_elements(input, 5), expected);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ cd.h
@@ -0,0 +1,8 @@
+﻿#include <vector>
+
+std::vector<int> increase_elements(std::vector<int>& vec, int n) {
+    for (int& element : vec) {
+        element += n;
+    }
+    return vec;
+}
